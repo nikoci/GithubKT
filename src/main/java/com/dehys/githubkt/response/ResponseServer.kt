fbafc4667b0ca984ecc386ba23e06d30a2e85dc4
dehys <arijan@nikoci.se>
2022-02-10 12:00:06 +0100

    starting to add ResponseAPI

--- src/main/java/com/dehys/githubkt/response/ResponseServer.kt
+++ src/main/java/com/dehys/githubkt/response/ResponseServer.kt
@@ -1,15 +1,20 @@
 package com.dehys.githubkt.response
 
 import com.dehys.githubkt.Github
+import fi.iki.elonen.NanoHTTPD
 
 @Suppress("unused")
-object ResponseAPI {
+class ResponseServer : NanoHTTPD {
 
+    constructor(host: String?, port: Int) : super(host, port)
+    constructor(port: Int) : super(port)
     init {
         if (Github.privateAccessToken == null) throw IllegalStateException("Github.privateAccessToken is not initialized")
         //init http servlet to listen for http requests from github webhooks
     }
 
+
+    
     // Able to connect webhooks to the api by addEventListener
 
 }
